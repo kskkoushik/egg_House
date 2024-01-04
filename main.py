2ed7fccc325b807701d974162a40faf2bda5d4f9
kskkoushik <114588668+kskkoushik@users.noreply.github.com>
2024-01-04 16:23:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template , request
+from mdb import take_tokens , sold_tokens , data , get_all_data
+
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def first():
+     sold_tokens = 0
+     value_sold = 0
+     all_users = get_all_data()
+     for user in all_users:
+       sold_tokens += user['sold']
+       value_sold += user['sold_val']
+     return render_template('mainPage.html' , sold_tokens = sold_tokens , value_sold = value_sold)
+
+@app.route('/take_submit',  methods = ['POST'])
+def taken_tok():
+   name = str(request.form['name'])
+   taken = int(request.form['tokens'])
+   type = str(request.form['token_type'])
+   name = name.upper()
+   take_tokens(name , taken , type)
+   return name + ' your stats updated sucessfully'
+
+@app.route('/sold_submit' , methods = ['POST'])
+def sold_tok():
+  name = str(request.form['name'])
+  sold = int(request.form['tokensold'])
+  type = str(request.form['token_type'])
+  name = name.upper()
+  i = sold_tokens(name , type , sold)
+  if i == -1 :
+    return name + ' your name is not found in team EggHouse'
+  else :
+    return name + ' your stats updated sucessfully'
+    
+@app.route('/view_submit' , methods = ['POST'])
+def view_data():
+   name = request.form['name']
+   details = data(name.upper())
+   return render_template('view.html' , details = details)
+   
+@app.route('/view_everyones_details' , methods= ['POST'])
+def list_gen():
+  users = get_all_data()
+  return render_template('viewlist.html' , users = users)
+
+if __name__ == "__main__" :
+
+   app.run(host = '0.0.0.0' , debug = True)
---
+++ mdb.py
@@ -0,0 +1,85 @@
+import flask
+import pymongo
+from flask import Flask
+from flask_pymongo import PyMongo
+from pymongo import MongoClient
+
+mongo_url = "mongodb+srv://radhakrishn1811:radhekrishna@cluster0.knjvanw.mongodb.net/?retryWrites=true&w=majority"
+
+
+
+member= MongoClient(mongo_url)
+
+db = member['member_stats']
+
+name = db['name']
+
+def value_pred(type):
+   if type == 'bo' :
+      return 50
+   elif type == 'boc' :
+     return 60
+   else :
+     return 80
+
+
+def take_tokens(taker_name , take_count , type):
+   mem = name.find_one({'name' : taker_name})
+
+   if mem is None:
+     name.insert_one({'name' : taker_name , 'tokens' : 0 , 'bo' : 0 , 'boc' : 0 , 'boch' : 0 , 'chiegg' : 0 , 'sold' : 0 , 'sold_val' : 0 , 'hold_val' : 0})
+     temp = name.find_one({'name' : taker_name})
+     count = temp[type]
+     count += take_count
+     val = temp['hold_val']
+     val = val + take_count*value_pred(type)
+     name.update_one({'name' : taker_name} , {'$set' :{type : count}})
+     name.update_one({'name' : taker_name} , {'$set' :{'value' : val }})
+   else :
+      temp = name.find_one({'name' : taker_name})
+      count = temp[type]
+      count += take_count
+      val = temp['hold_val']
+      val = val + take_count*value_pred(type)
+      name.update_one({'name' : taker_name} , {'$set' :{type : count}})
+      name.update_one({'name' : taker_name} , {'$set' :{'hold_val' : val }})
+     
+
+def sold_tokens(seller_name , type , sell_count):
+   mem = name.find_one({'name' : seller_name})
+
+   if mem is None:
+      name.insert_one({'name' : seller_name , 'tokens' : 0 , 'bo' : 0 , 'boc' : 0 , 'boch' : 0 , 'chiegg' : 0 , 'sold' : 0 , 'sold_val' : 0 , 'hold_val' : 0})
+      temp = name.find_one({'name' : seller_name})
+      count = mem[type]
+      count = count - sell_count
+      sol_count = mem['sold'] + sell_count
+      val = mem['hold_val'] - (value_pred(mem[type])*sell_count)
+      sval = mem['sold_val'] + (value_pred(mem[type])*sell_count)
+      name.update_one({'name' : seller_name} , {'$set' :{type : count}})
+      name.update_one({'name' : seller_name} , {'$set' :{'sold' : sol_count}})
+      name.update_one({'name' : seller_name} , {'$set' :{'sold_val' : sval}})
+      name.update_one({'name' : seller_name} , {'$set' :{'hold_val' : val}})
+     
+      
+   else :
+     temp = mem
+     count = mem[type]
+     count = count - sell_count
+     sol_count = mem['sold'] + sell_count
+     val = mem['hold_val'] - (value_pred(mem[type])*sell_count)
+     sval = mem['sold_val'] + (value_pred(mem[type])*sell_count)
+     name.update_one({'name' : seller_name} , {'$set' :{type : count}})
+     name.update_one({'name' : seller_name} , {'$set' :{'sold' : sol_count}})
+     name.update_one({'name' : seller_name} , {'$set' :{'sold_val' : sval}})
+     name.update_one({'name' : seller_name} , {'$set' :{'hold_val' : val}})
+     
+
+def data(find_name):
+   return name.find_one({'name' : find_name})
+
+def get_all_data():
+
+  return name.find()
+
+
